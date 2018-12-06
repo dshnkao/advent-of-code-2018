d08874e2914449d84d1ccd78158b690e1893d7df
Dennis Kao <dshnkao@gmail.com>
2018-12-06 21:19:23 +1100

    day5b

--- src/day5.rs
+++ src/day5.rs
@@ -4,10 +4,11 @@ fn is_reactive(a: &char, b: &char) -> bool {
     a != b && (a.to_ascii_lowercase() == *b || b.to_ascii_lowercase() == *a)
 }
 
-pub fn part1(input: &str) -> Result<usize> {
+fn react_polymer(polymer: &str, ignore_chars: &[char]) -> Vec<char> {
     let mut reacted_polymer: Vec<char> = Vec::new();
-    input.chars()
+    polymer.chars()
         .filter(|x| x.is_alphabetic())
+        .filter(|x| !ignore_chars.into_iter().any(|c| c == x))
         .for_each(|cur| {
             match reacted_polymer.last().cloned() {
                 Some(prv) if is_reactive(&prv, &cur) => drop(reacted_polymer.pop()),
@@ -15,9 +16,19 @@ pub fn part1(input: &str) -> Result<usize> {
                 None => reacted_polymer.push(cur),
             }
         });
-    Ok(reacted_polymer.len())
+    reacted_polymer
+}
+
+pub fn part1(input: &str) -> Result<usize> {
+    Ok(react_polymer(&input, &[]).len())
 }
 
 pub fn part2(input: &str) -> Result<usize> {
-    Ok(1)
+    let polymer = (b'a' ..= b'z')
+        .map(|c| c as char)
+        .map(|c| react_polymer(&input, &[c, c.to_ascii_uppercase()]))
+        .min_by_key(|polymer| polymer.len())
+        .ok_or("no polymer found")?;
+
+    Ok(polymer.len())
 }
